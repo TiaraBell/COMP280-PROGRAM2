61c4e67e1b9c493adae7de9dd8ac9b438de7fb9a
Tiara <tbell1@aggies.ncat.edu>
2019-06-02 16:33:52 -0400

    add project start

---
+++ List/List.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   List.cpp
+ * Author: tiara
+ * 
+ * Created on June 2, 2019, 4:32 PM
+ */
+
+#include "List.h"
+
+List::List() {
+}
+
+List::List(const List& orig) {
+}
+
+List::~List() {
+}
+
---
+++ List/List.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   List.h
+ * Author: tiara
+ *
+ * Created on June 2, 2019, 4:32 PM
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
+class List {
+public:
+    List();
+    List(const List& orig);
+    virtual ~List();
+private:
+
+};
+
+#endif /* LIST_H */
+
